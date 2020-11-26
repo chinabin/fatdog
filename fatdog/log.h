5fa17470cbb8025fa24a8c96f7582f1948fdaae0
ubuntu <ubuntu@localhost.localdomain>
2020-11-26 15:58:10 +0800

    init

---
+++ fatdog/log.h
@@ -0,0 +1,13 @@
+#ifndef __FATDOG_LOG__
+#define __FATDOG_LOG__
+
+/*
+Logger: 负责产生日志信息，接收端
+LogAppender: 负责将日志信息落地，例如文件、输出窗口，输出端
+LogFormatter: 负责格式化日志信息，处于接收端到输出端的中间
+*/
+namespace fatdog{
+
+}
+
+#endif
